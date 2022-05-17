dac96cd70b53aebd7ab16ac2f736ba786879ea27
belueu <belu.development@gmail.com>
2022-05-17 10:11:10 +0300

    Added examples of Generics, bounded types, Covariace, Invariance, Contravariance
    Modified the MyList exercise to be of generic type

--- src/main/scala/exercises/MyList.scala
+++ src/main/scala/exercises/MyList.scala
@@ -1,6 +1,6 @@
 package exercises
 
-abstract class MyList {
+abstract class MyList[+A] {
 
   /*
     head = first element of the list
@@ -10,13 +10,13 @@ abstract class MyList {
     toString => a string representation of the list
   * */
 
-  def head: Int
+  def head: A
 
-  def tail: MyList
+  def tail: MyList[A]
 
   def isEmpty: Boolean
 
-  def add(element: Int): MyList
+  def add[B >: A](element: B): MyList[B]
 
   def printElements: String
 
@@ -24,40 +24,38 @@ abstract class MyList {
   override def toString: String = "[" + printElements + "]"
 }
 
-object Empty extends MyList {
-  override def head: Int = throw new NoSuchElementException()
+object Empty extends MyList[Nothing] {
+  override def head: Nothing = throw new NoSuchElementException()
 
-  override def tail: MyList = throw new NoSuchElementException()
+  override def tail: MyList[Nothing] = throw new NoSuchElementException()
 
   override def isEmpty: Boolean = true
 
-  override def add(element: Int): MyList = new Cons(element, Empty)
+  override def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
 
   override def printElements: String = ""
 }
 
-class Cons(h: Int, t: MyList) extends MyList {
-  override def head: Int = h
+class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
+  override def head: A = h
 
-  override def tail: MyList = t
+  override def tail: MyList[A] = t
 
   override def isEmpty: Boolean = false
 
-  override def add(element: Int): MyList = new Cons(element, this)
+  override def add[B >: A](element: B): MyList[B] = new Cons(element, this)
 
   override def printElements: String = {
     if (t.isEmpty) "" + h
-    else h + " " + t.printElements
+    else s"$h ${t.printElements}"
   }
 }
 
 object ListTest extends App {
-  val list = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  val listOfIntegers: MyList[Int] = new Cons[Int](1, new Cons[Int](2, new Cons[Int](3, Empty)))
+  val listOfStrings: MyList[String] = new Cons[String]("hello", new Cons[String]("Scala", Empty))
 
-  println(list.head)
-  println(list.tail.head)
-  println(list.add(4).head)
-  println(list.isEmpty)
+  println(listOfIntegers.toString)
+  println(listOfStrings.toString)
 
-  println(list.toString)
 }
